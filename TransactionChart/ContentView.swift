7be30d297cae1ca46818a058b4c6be9383363daf
Yemi Gabriel <yemigabriel@gmail.com>
2022-03-14 06:48:35 +0100

    Initial Commit

---
+++ TransactionChart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TransactionChart
+//
+//  Created by Yemi Gabriel on 3/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TransactionChart/TransactionChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransactionChartApp.swift
+//  TransactionChart
+//
+//  Created by Yemi Gabriel on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct TransactionChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
